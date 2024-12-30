3bc4f64fc78c703c6b8da51b91f49553ac7bdad8
mf751f <mf751f@gmail.com>
2024-12-30 23:04:36 +0300

    adding routes and handlers for user authinticating and signining up

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -101,3 +101,23 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Display a HTML form for sigining up a new user")
+}
+
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create a new user")
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Display a HTML form for logging in a user")
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Authinticate and login the user")
+}
+
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Logout the user... ")
+}
