bc8392c9725328f78ca9e86a08c4cf892eab078c
mf751f <mf751f@gmail.com>
2024-12-30 21:17:14 +0300

    tls

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -54,6 +54,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = postgresstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := application{
 		infoLog:        infoLog,
@@ -71,7 +72,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %v", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errLog.Fatal(err)
 }
 
