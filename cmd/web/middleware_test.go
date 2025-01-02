0e2cc5fd4d181a675fe108b5238b5cc361b94e79
mf751f <mf751f@gmail.com>
2025-01-02 23:14:14 +0300

    testing middleware

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mf751f/snippetbox/internal/assert"
+)
+
+func TestSecureHeaders(t *testing.T) {
+	responseRecorder := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	secureHeaders(next).ServeHTTP(responseRecorder, request)
+	result := responseRecorder.Result()
+
+	expectedValue := "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com"
+	assert.Equal(t, result.Header.Get("Content-Security-Policy"), expectedValue)
+
+	expectedValue = "nosniff"
+	assert.Equal(t, result.Header.Get("X-Content-Type-Options"), expectedValue)
+
+	expectedValue = "deny"
+	assert.Equal(t, result.Header.Get("X-Frame-Options"), expectedValue)
+
+	expectedValue = "0"
+	assert.Equal(t, result.Header.Get("X-XSS-Protection"), expectedValue)
+
+	assert.Equal(t, result.StatusCode, http.StatusOK)
+
+	defer result.Body.Close()
+	body, err := io.ReadAll(result.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
